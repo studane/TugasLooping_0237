e6f4276d40b29d77878495854e981266f35fdbba
studane <zhidaneprabowoguzlend@gmail.com>
2026-03-30 20:28:55 +0700

    tambah while loop

--- TugasLooping.cpp
+++ TugasLooping.cpp
@@ -21,3 +21,12 @@ bool cekPrima(int x) {
     if (x <= 1)
         return false;
     }
+
+int i = 2;
+    while (i < x) {
+        if (x % i == 0) {
+            return false;
+        }
+        i++;
+    }
+    return true;
