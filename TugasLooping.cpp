2cc1444f9666ed8062a6e7bbdcb7aee59d4a4c65
studane <zhidaneprabowoguzlend@gmail.com>
2026-03-30 20:10:22 +0700

    membuat fungsi menu

--- TugasLooping.cpp
+++ TugasLooping.cpp
@@ -3,3 +3,11 @@ using namespace std;
 
 int n;
 int pilihan;
+
+void tampilMenu() {
+    cout << "\n===== MENU PROGRAM =====\n";
+    cout << "1. Cek Bilangan Prima\n";
+    cout << "2. Cek Bilangan Fibonacci\n";
+    cout << "0. Keluar\n";
+    cout << "Pilih menu: ";
+}
